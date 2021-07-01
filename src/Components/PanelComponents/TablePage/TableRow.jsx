208a3e08ce01fdb5cc370a81dd65bd27b57949b0
maguilococo <56405247+maguilococo@users.noreply.github.com>
2021-07-01 13:42:47 -0300

    progress in responsive

--- src/Components/PanelComponents/TablePage/TableRow.jsx
+++ src/Components/PanelComponents/TablePage/TableRow.jsx
@@ -8,30 +8,30 @@ export default function TableRow({
   path, id, buttonPath, deleteAction, userId
 }) {
   return (
-    <tr>
-      <td>{column1}</td>
-      {displayLink
-        ? (
-          <td>
-            <NavLink to={`/panel/detail/${path}/${link}`} className={style.link}>
-              {column2}
-            </NavLink>
-          </td>
-        )
-        : (
-          <td>
-            { column2 }
-          </td>
-        )}
-      <td style={{ textAlign: 'right'}}>{column3}</td>
-      <td>
-        <ButtonOptions
-          userId={userId}
-          deleteAction={deleteAction}
-          buttonPath={buttonPath}
-          id={id}
-        />
-      </td>
-    </tr>
+      <tr>
+        <td>{column1}</td>
+        {displayLink
+          ? (
+            <td>
+              <NavLink to={`/panel/detail/${path}/${link}`} className={style.link}>
+                {column2}
+              </NavLink>
+            </td>
+          )
+          : (
+            <td>
+              { column2 }
+            </td>
+          )}
+        <td >{column3}</td>
+        <td>
+          <ButtonOptions
+            userId={userId}
+            deleteAction={deleteAction}
+            buttonPath={buttonPath}
+            id={id}
+          />
+        </td>
+      </tr>
   );
 }
